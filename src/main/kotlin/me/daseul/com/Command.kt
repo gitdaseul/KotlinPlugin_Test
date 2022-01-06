f79dbe1b5b39dc27ed122d28cc7946cb64b0f358
gitdaseul <daseul035@gmail.com>
2022-01-06 15:59:31 +0900

    오류수정 0.5v | 빌드

--- src/main/kotlin/me/daseul/com/Command.kt
+++ src/main/kotlin/me/daseul/com/Command.kt
@@ -4,7 +4,6 @@ import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
 import org.bukkit.command.TabExecutor
 import org.bukkit.entity.Player
-import org.bukkit.event.player.PlayerAdvancementDoneEvent
 
 class Command: TabExecutor {
 
@@ -47,24 +46,6 @@ class Command: TabExecutor {
                 }
             }
         }
-        if(sender is Player){
-            val p: Player = sender
-            if(args.size > 0){
-                if(args[0] == "build"){
-                    if(args[1] == "code"){
-                        if(args[2] == readLine()){
-                            var input = args[2]
-                            p.sendMessage(input + "을(를) Build 했습니다!")
-                            p.sendTitle(p.health.toString(), "완료!", 0, 15, 0)
-                            p.sendMessage("By Daseul")
-                        }
-                    }
-                    if(args[1] == "info"){
-                        p.sendMessage("(/)Build 명령어는 MKP_1의 자체 제작 Build Tool입니다")
-                    }
-                }
-            }
-        }
         return false
     }
 
@@ -87,13 +68,11 @@ class Command: TabExecutor {
         TabCommand1?.add("info")
         TabCommand1?.add("game")
         TabCommand1?.add("kotlin")
-        TabCommand1?.add("build")
         return TabCommand1
     }
     fun TabCommand2(): MutableList<String>? {
         TabCommand2?.add("start")
         TabCommand2?.add("end")
-        TabCommand2?.add("code")
         return TabCommand2
     }
 }
